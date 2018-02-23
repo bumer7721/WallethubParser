556ae3425dc837c2caa6ff7d04b69bd921981442
Volodimir Gladkyy <vgladkyy@vf-soft.com>
2018-02-23 16:02:28 +0200

    initial repository

---
+++ src/main/java/com/ef/Parser.java
@@ -0,0 +1,19 @@
+package com.ef;
+
+import java.util.Scanner;
+
+public class Parser {
+	public static void main(String [] args)
+	{
+		System.out.println("Hello from Parser!!!");
+		
+		promptEnterKey();
+		System.exit(0);
+	}
+	
+	private static void promptEnterKey(){
+	   System.out.println("Press \"ENTER\" to exit...");
+	   Scanner scanner = new Scanner(System.in);
+	   scanner.nextLine();
+	}
+}
---
+++ src/test/java/com/ef/ParserTest.java
@@ -0,0 +1,12 @@
+package com.ef;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ParserTest {
+    
+	@Ignore
+	@Test 
+    public void testSomeLibraryMethod() {
+        Parser classUnderTest = new Parser();
+    }
+}
